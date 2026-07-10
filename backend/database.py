cc57a6f04ab3647cb5e037ece2d46b74e3facf70
Dharani Chetagala <dharanichetagala@gmail.com>
2026-07-10 19:21:26 +0530

    Added WasteInventory model

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,23 @@ import sqlite3
 
 conn = sqlite3.connect("textile_waste.db")
 
-print("Database Connected Successfully")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS waste_inventory (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    batch_id TEXT,
+    fabric_type TEXT,
+    source TEXT,
+    quantity INTEGER,
+    color TEXT,
+    condition TEXT,
+    collection_date TEXT
+)
+""")
+
+conn.commit()
+
+print("Database and Table Created Successfully")
 
 conn.close()
--- backend/models.py
+++ backend/models.py
@@ -1,2 +1,12 @@
 class WasteInventory:
-    pass
+    def __init__(self, batch_id, fabric_type, source,
+                 quantity, color, condition,
+                 collection_date):
+
+        self.batch_id = batch_id
+        self.fabric_type = fabric_type
+        self.source = source
+        self.quantity = quantity
+        self.color = color
+        self.condition = condition
+        self.collection_date = collection_date
